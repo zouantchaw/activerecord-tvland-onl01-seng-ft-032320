e38bde89ff853ef533b111a4392265f131b83df2
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-27 19:15:02 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.collect do |character|
+          Actor.find_by(id:character.actor_id).full_name
+        end
+
+    end
 end
